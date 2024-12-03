d97bd746c167c2c63d39157716546910aa5790f1
Matrix-X <hbj418@gmail.com>
2024-12-03 11:35:24 +0800

    refact(model): media resource and user fields for brainx

--- internal/model/media/mediaresource.go
+++ internal/model/media/mediaresource.go
@@ -4,21 +4,24 @@ import (
 	"PowerX/internal/model"
 	"PowerX/internal/model/powermodel"
 	"PowerX/internal/types"
+	"github.com/google/uuid"
 )
 
 type MediaResource struct {
 	powermodel.PowerUUIDModel
 
-	CustomerId    int64  `gorm:"comment:客户Id; index" json:"customerId"`
-	Filename      string `gorm:"comment:名称" json:"filename"`
-	Size          int64  `gorm:"comment:尺寸" json:"size"`
-	Width         int64  `gorm:"comment:宽度" json:"width"`
-	Height        int64  `gorm:"comment:长度" json:"height"`
-	Url           string `gorm:"comment:url" json:"url"`
-	BucketName    string `gorm:"comment:Bucket名称" json:"bucketName"`
-	IsLocalStored bool   `gorm:"comment:是否本地存储" json:"isLocalStored"`
-	ContentType   string `gorm:"comment:内容类型" json:"contentType"`
-	ResourceType  string `gorm:"comment:媒体类型" json:"mediaType"`
+	TenantUUID    uuid.UUID `gorm:"type:uuid;foreignKey:TenantUUID;references:UUID"`
+	CreatedUserBy uuid.UUID `gorm:"type:uuid;foreignKey:CreatedUserBy;references:UUID;not null"`
+	CustomerId    int64     `gorm:"comment:客户Id; index" json:"customerId"`
+	Filename      string    `gorm:"comment:名称" json:"filename"`
+	Size          int64     `gorm:"comment:尺寸" json:"size"`
+	Width         int64     `gorm:"comment:宽度" json:"width"`
+	Height        int64     `gorm:"comment:长度" json:"height"`
+	Url           string    `gorm:"comment:url" json:"url"`
+	BucketName    string    `gorm:"comment:Bucket名称" json:"bucketName"`
+	IsLocalStored bool      `gorm:"comment:是否本地存储" json:"isLocalStored"`
+	ContentType   string    `gorm:"comment:内容类型" json:"contentType"`
+	ResourceType  string    `gorm:"comment:媒体类型" json:"mediaType"`
 }
 
 type MediaSet struct {
